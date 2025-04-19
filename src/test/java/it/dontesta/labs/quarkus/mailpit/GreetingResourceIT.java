e4dfd453591c7a26e7e260d74ba7ffe949a9d419
Antonio Musarra <antonio.musarra@gmail.com>
2025-04-19 12:22:17 +0200

    Rimozione classi autogenerate

--- src/test/java/it/dontesta/labs/quarkus/mailpit/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package it.dontesta.labs.quarkus.mailpit;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
