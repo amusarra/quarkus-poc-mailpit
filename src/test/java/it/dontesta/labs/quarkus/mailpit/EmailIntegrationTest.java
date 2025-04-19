e6d12181b44fe70ad6d0fe16d14f915813e16845
Antonio Musarra <antonio.musarra@gmail.com>
2025-04-19 21:18:13 +0200

    Aggiunta estensione quarkus-mailpit: 1. aggiunta estensione io.quarkiverse.mailpit:quarkus-mailpit 2. aggiunta estensione io.quarkiverse.mailpit:quarkus-mailpit-testing 3. revisione EmailIntegrationTest

--- src/test/java/it/dontesta/labs/quarkus/mailpit/EmailIntegrationTest.java
+++ src/test/java/it/dontesta/labs/quarkus/mailpit/EmailIntegrationTest.java
@@ -2,13 +2,31 @@ package it.dontesta.labs.quarkus.mailpit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
+import io.quarkiverse.mailpit.test.InjectMailbox;
+import io.quarkiverse.mailpit.test.Mailbox;
+import io.quarkiverse.mailpit.test.WithMailbox;
+import io.quarkiverse.mailpit.test.invoker.ApiException;
+import io.quarkiverse.mailpit.test.model.Message;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
+@WithMailbox
 public class EmailIntegrationTest {
+
+  @InjectMailbox
+  Mailbox mailbox;
+
+  @AfterEach
+  public void afterEach() throws ApiException {
+    // clear the mailbox after each test run if you prefer
+    mailbox.clear();
+  }
 
   @Test
   void shouldSendEmailAndVerifyInMailpit() throws InterruptedException {
@@ -18,18 +36,11 @@ public class EmailIntegrationTest {
     // Attendi che Mailpit riceva l'email
     Thread.sleep(1000);
 
-    // Step 2: Recupera l'ultima email da Mailpit
-    var response = RestAssured
-        .get("http://localhost:8025/api/v1/messages")
-        .then()
-        .statusCode(200)
-        .extract()
-        .jsonPath();
-
-    var subject = response.getString("messages[0].Subject");
-    var body = response.getString("messages[0].Snippet");
-
-    assertThat(subject, containsString("Ciao da Quarkus"));
-    assertThat(body, containsString("email di test"));
+    // look up the mail and assert it
+    Message message = mailbox.findFirst("no-reply@example.com");
+    assertThat(message, notNullValue());
+    assertThat(message.getTo().get(0).getAddress(), is("test@example.com"));
+    assertThat(message.getSubject(), is("Ciao da Quarkus!"));
+    assertThat(message.getText(), containsString("email di test"));
   }
 }
